b4b8ce5c550c8ef6c92c196a8dd8a8ec62d93060
Nathan Chambers <nate@webenguk.com>
2015-08-13 03:28:41 +0100

    Update of default.py, rss feed update and removal of feed parser

--- README.md
+++ README.md
@@ -12,19 +12,6 @@ Watch all the episodes from Category 5 at your pleasure. (Live show streaming co
       * Load Kodi and go to system and find "Add-Ons" on the left hand side,
       * Click "Install from zip file",
       * Locate the feedparser zip and click the file, then press ok.  
-      
-      PLEASE NOTE: You need to install the requirements for this script to work.
-
-## Requirements
-
-feedparser:
-
-    install the feedparser script by:
-
-      * Downloading the ZIP FILE from https://github.com/dersphere/script.module.feedparser
-        (make sure you put it somewhere you can find it later),
-      * Load Kodi and go to system and find "Add-Ons" on the left hand side,
-      * Click "Install from zip file",
-      * Locate the feedparser zip and click the file, then press ok.  
+       
 
 Please visit http://www.category5.tv/
--- default.py
+++ default.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 import sys
-import urllib
 import urlparse
 import xbmc
 import xbmcgui
 import xbmcplugin
-import os
-import feedparser
-
+import urllib
+import urllib2,cookielib
+import re
 
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
@@ -16,31 +15,27 @@ args = urlparse.parse_qs(sys.argv[2][1:])
 xbmcplugin.setContent(addon_handle, 'plugin.video.category5')
 cat5Shows = {}
 
-feedsURL = "http://rss.cat5.tv/"
 feedsImage = "http://cdn.zecheriah.com/img/rss/sd/icon-144.jpg"
 
 cat5Shows[0] = {
     'cat5Folder': "Full Episodes HD",
     'cat5Title': "Full Episodes HD",
     'cat5Image': feedsImage,
-    'cat5Feed': feedsURL + "hd.rss"
-
+    'cat5Feed': "http://rss.cat5.tv/kodi/tech-hd.rss"
 }
 
 cat5Shows[1] = {
     'cat5Folder': "Full Episodes SD",
     'cat5Title': "Full Episodes SD",
     'cat5Image': feedsImage,
-    'cat5Feed': feedsURL + "sd.rss"
-
+    'cat5Feed': "http://rss.cat5.tv/kodi/tech-sd.rss"
 }
 
 cat5Shows[2] = {
     'cat5Folder': "Full Episodes LD",
     'cat5Title': "Full Episodes LD",
     'cat5Image': feedsImage,
-    'cat5Feed': feedsURL + "ld.rss"
-
+    'cat5Feed': "http://rss.cat5.tv/kodi/tech-ld.rss"
 }
 
 def build_url(query):
@@ -54,21 +49,33 @@ def addfolders(url, title, image):
     return xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
 def feedrss(feedrssurl):
+    
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'}
+    
+    rssurl = urllib2.Request(feedrssurl, headers=hdr)
+    response = urllib2.urlopen(rssurl)
+    
+    sourceCode = response.read()
+    
+    numberrss = re.findall(r'<cat5tv:number>(.*?)</cat5tv:number>', sourceCode)
+    titlerss = re.findall(r'<cat5tv:title>(.*?)</cat5tv:title>', sourceCode)
+    thumbnailrss = re.findall(r'<cat5tv:thumbnail>(.*?)</cat5tv:thumbnail>', sourceCode)
+    linksrss = re.findall(r'<link>(.*?).m4v</link>', sourceCode)
+    descriptionrss = re.findall(r'<cat5tv:description>(.*?)</cat5tv:description>', sourceCode)
 
-    feeds = feedparser.parse( feedrssurl )
-
-    for feed in feeds[ "items" ]:
-
-        url = feed[ "link" ]
-        title = feed[ "title" ]
-        title = title.replace('Category5 Technology TV - ', '')
+    for rssnumber, rsstitle, rssthumbnail, rsslinks, rssdescription in zip(numberrss, titlerss, thumbnailrss, linksrss, descriptionrss):
         
-        image = title.split(' - ')
-        image2 = image[0].split('Episode ')
-        image3 = "http://cdn3.taliferguson.com/img/tech/" + image2[1] + "/200.jpg"
+        url = rsslinks
+        title = rssnumber + ' - ' + rsstitle
         
-        li = xbmcgui.ListItem(title, iconImage=image3)
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+        li = xbmcgui.ListItem(title, iconImage=rssthumbnail)
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
+
 
     return xbmcplugin.endOfDirectory(addon_handle)
 
